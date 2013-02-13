dd31927b15c2b17affbec62c59d1739af4d75960
benjaminabbitt <benjamin.abbitt@gmail.com>
2013-02-12 21:41:43 -0600

    Added credit to FGreg/Stackoverflow

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ slf4jInjector
 =============
 
 Injection framework to using Spring for logging.
+
+Credit for this is due to stackoverflow user FGreg.  This is a humble adaption of his work.  See the thread at http://stackoverflow.com/questions/6351082/using-java-annotation-to-inject-logger-dependency
--- src/main/java/com/github/benjaminAbbitt/slf4jInjector/SLF4JPostProcessor.java
+++ src/main/java/com/github/benjaminAbbitt/slf4jInjector/SLF4JPostProcessor.java
@@ -1,9 +1,10 @@
 package com.github.benjaminAbbitt.slf4jInjector;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.List;
+/*
+Credit for this work is due to Stackoverflow's FGReg and this flag:
+http://stackoverflow.com/questions/6351082/using-java-annotation-to-inject-logger-dependency
+
+ */
 
 import com.github.benjaminAbbitt.logging.injectLogger.InjectLogger;
 import org.slf4j.Logger;
@@ -11,6 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class SLF4JPostProcessor implements BeanPostProcessor {
@@ -29,7 +35,7 @@ public class SLF4JPostProcessor implements BeanPostProcessor {
 
                 logger.debug("Attempting to inject a SLF4J logger on bean: " + bean.getClass());
 
-                if (field != null && (field.getModifiers() & Modifier.STATIC) == 0) {
+                if ((field.getModifiers() & Modifier.STATIC) == 0) {
                     field.setAccessible(true);
                     try {
                         field.set(bean, LoggerFactory.getLogger(bean.getClass()));
